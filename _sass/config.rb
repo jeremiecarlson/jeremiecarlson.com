89bd80fe5cdd067daca1ad750e9e6c3d3cb0a2e3
Jeremie Carlson <findme@jeremiecarlson.com>
2019-01-30 22:02:38 -0800

    gumming up the works

--- _sass/config.rb
+++ _sass/config.rb
@@ -1,13 +1,12 @@
+require 'compass/import-once/activate'
 # Require any additional compass plugins here.
 
 # Set this to the root of your project when deployed:
 http_path = "/"
-sass_dir = "./"
 css_dir = "../css"
-images_dir = "../img"
+sass_dir = "./"
+images_dir = "images"
 javascripts_dir = "../js"
-relative_assets = true
-output_style = :compressed
 
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
